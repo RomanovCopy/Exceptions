40d308f6e245ffb9ec4d5cf4c51ba3b422fd8687
RomanovCopy <RomanovCopy@gmail.com>
2023-09-23 15:56:27 +0300

    task_2

---
+++ Exceptions/src/com/romanovCopy/task_2/Solution.java
@@ -0,0 +1,39 @@
+package task_2;
+
+public class Solution {
+
+    /* Исходный код */
+
+//    try {
+//        int d = 0;
+//        double catchedRes1 = intArray[8] / d;
+//        System.out.println("catchedRes1 = " + catchedRes1);
+//    } catch (ArithmeticException e) {
+//        System.out.println("Catching exception: " + e);
+//    }
+
+    /* Ошибки */
+
+    // 1 Массив intArray не инициализирован, ошибка компиляции
+    // 2 Переменная d инициализирована значением 0, что приведет к возникновению
+    //  исключения ArithmeticException при попытке деления на ноль.
+    // 3 В блоке catch необходимо указать тип исключения, которое мы хотим перехватить.
+    //   В данном случае, указан тип ArithmeticException, что позволит перехватить
+    //  исключение, возникающее при делении на ноль.
+    // 4 Выводимое сообщение в блоке catch может быть более информативным,
+    // чтобы помочь в диагностике и отладке проблемы.
+
+    /*Исправленный код*/
+
+    try {
+        int[] intArray = new int[10];
+        int d = 0;
+        double catchedRes1 = intArray[8] / d;
+        System.out.println("catchedRes1 = " + catchedRes1);
+    } catch (ArithmeticException e) {
+        System.out.println("Caught exception: " + e.getMessage());
+    }
+
+
+
+}
